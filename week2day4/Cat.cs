cd5c39d0f51bd5dfb9a8a4c36d571ddf9255d7b1
KaitieFox <kaitiefox@gmail.com>
2017-10-05 15:14:31 -0400

    Cat

--- week2day4/Cat.cs
+++ week2day4/Cat.cs
@@ -8,5 +8,35 @@ namespace week2day4
 {
     class Cat
     {
+        //STATES
+        //two types of states:: 
+        //::fields
+        private string name;
+        
+        //::properties
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+
+        }
+
+
+        //BEHAVIOURS
+        
+
+        //Constructors
+        public Cat()
+        {
+            //default constructor because it takes no parameters in parentheses
+            //inside curly braces are called body.
+            //default constructor title hinges on parentheses.
+            //can still have stuff in body
+
+
+        }
+
+
+
     }
 }
